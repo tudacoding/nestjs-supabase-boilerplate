5bc4ece872b9a5c357f70c88ef1d3500aa620fc1
Thinh Nguyen <thinhnguyen.teamx@gmail.com>
2025-03-25 14:24:49 +0700

    add supabase module to course

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CoursesController } from './courses.controller';
 import { CoursesService } from './courses.service';
+import { ConfigModule } from '@nestjs/config';
+import { SupabaseModule } from 'src/supabase/supabase.module';
 
 @Module({
+  imports: [ConfigModule, SupabaseModule],
   controllers: [CoursesController],
   providers: [CoursesService],
 })
